d9c9d492d981d5e4cf75b0f63e645ffde437e9a5
jerlin-joy <jerlinjoy2017@gmail.com>
2025-05-19 22:06:38 +0530

    Add Cloudflare middleware to inject country

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,21 +1,21 @@
-export default {
-    async fetch(request) {
-        const countryCode = request.headers.get('CF-IPCountry') || 'US';
-        const response = await fetch(request);
-        const contentType = response.headers.get('Content-Type') || '';
+export async function onRequest({ request, next }) {
+    const countryCode = request.headers.get('CF-IPCountry') || 'US';
 
-        if (contentType.includes('text/html')) {
-            const html = await response.text();
-            const injected = html.replace(
-                '</head>',
-                `<script>window.__COUNTRY__ = '${countryCode}';</script></head>`
-            );
-            return new Response(injected, {
-                status: response.status,
-                headers: { 'Content-Type': 'text/html' }
-            });
-        }
+    const response = await next();
+    const contentType = response.headers.get('content-type') || '';
 
-        return response;
+    if (contentType.includes('text/html')) {
+        const html = await response.text();
+        const injectedHtml = html.replace(
+            '</head>',
+            `<script>window.__COUNTRY__ = '${countryCode}';</script></head>`
+        );
+
+        return new Response(injectedHtml, {
+            status: response.status,
+            headers: response.headers
+        });
     }
-};
+
+    return response;
+}
